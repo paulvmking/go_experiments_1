b569a3573215abb2d6b2912ca558722f418ba188
paulvmking <paulvmking@gmail.com>
2022-10-10 20:44:19 +0000

    Import libraries and writing main body of function

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ This code is my solution for a coursera problem.
 package main
 
 import ( // imports libraries 
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
 )
 
 type Result struct { //creating a struct for use with a channel 
@@ -24,3 +31,50 @@ type Result struct { //creating a struct for use with a channel
 	Day		string	`json:"day"`
 }
 
+const Url = "https://xkcd.com" //storing url in global variable 
+
+func fetch(n int) (*Result, error) { 
+
+	client := &http.Client{
+		Timeout: 5 * time.Minute, //setting timeout for the client
+	}
+
+	// concatenate strings to get url; ex: https://xkcd.com/571/info.0.json
+	url := strings.Join([]string{Url, fmt.Sprintf("%d", n), "info.0.json"}, "/")
+
+	req, err := http.NewRequest("GET", url, nil) //get request and accompanying error message 
+
+	if err != nil { //if error not equal to nil 
+		return nil, fmt.Errorf("http request: %v", err)
+	}
+
+	resp, err := client.Do(req) //client executes request 
+	if err != nil { //if error nopt equal to nil 
+		return nil, fmt.Errorf("http err: %v", err)
+	}
+	
+	var data Result //setting the result as a local variable 
+	
+	// error from web service, empty struct to avoid disruption of process
+	if resp.StatusCode != http.StatusOK {
+		data = Result{
+		}
+		} else {
+			if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+				return nil, fmt.Errorf("json err: %v", err) //returning error in format 
+			}
+		}
+		
+	resp.Body.Close() //closing the body 
+
+	return &data, nil //returning the result
+}
+
+func main() {
+	n := 200 //assigning a value to "n" 
+	result, err := fetch(n) //calling the funtion to execute providing n as parameter 
+	if err != nil {
+		log.Fatal(err) //logging the error if it is fatal 
+	}
+	fmt.Printf("%v\n", result.Title) //printing the result 
+}
